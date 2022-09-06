a43df9a14ba22c8e28a7a396e13b3df5a7c2baff
hong <dlfemdrmq329@naver.com>
2022-09-06 22:14:48 +0900

    [update] 구독자,채널명,조회수,좋아요

---
+++ utube_crawling/test.py
@@ -0,0 +1,8 @@
+text ={"@context":"https://schema.org","@type":"VideoObject","description":"영상 편집자에게 미쳐버린 노트북이 출시했습니다. 지난 M1에서 더욱 고성능으로 만들어진 M1 Pro와 14인치/16인치로 풀체인지된 맥북프로 \
+가 출시하여 해외직구 후 간단히 언빡싱하면서 외관을 살펴봤습니다.\n\n◆ Spec. \n모델명 : 애플 맥북프로 14인치\n디스플레이 : 14.2인치 3024x1964 Liquid Retina XDR Display\n프로세서 : 애플 M1 Pro(10 CPU/16GPU)\nRAM : 16GB\n저장공간 : 1TB SSD\n무게 : 1.6kg\n가격 : 2,690,000원 \
+~\n\n\n◆ Music. \nES_Entropy - Gregory David\n\n\n◆ Tag. \n#맥북프로14인치 #M1Pro #언빡싱\n\n\n◆ Merch.\n지금 입고있는 옷? : https://abit.ly/blackcowlabs\n\n\n◆ ITSub 잇섭. \nE-MAIL - itsub@itud.io\nFACEBOOK - https://www.facebook.com/ITSubb/ \nINSTAGRAM - https://www.instagram.com/itsubb/\n\n\n◆ Timestamp. \
+\n00:00 매드무비\n00:12 오프닝 \n00:50 언빡싱\n02:44 디자인 첫 인상\n04:43 디스플레이 첫 인상\n06:38 달라진 키보드&스피커\n07:55 Geekbench 5\n08:26 14인치 vs 16인치\n09:34 CPU와 RAM 조합은?\n10:37 저도 Mac에 도전해볼\
+까요?\n12:17 그냥 오늘 영상에 대한 짧은 이야기?","duration":"PT812S","embedUrl":"https://www.youtube.com/embed/QndOyQtTHUQ","interactionCount":"1238176","name":"애플치고는 굉장히 못생겼다. M1 Pro 맥 \
+북프로 14인치 언빡싱&내가 선택한 옵션은?","thumbnailUrl":["https://i.ytimg.com/vi/QndOyQtTHUQ/maxresdefault.jpg"],"uploadDate":"2021-11-08","genre":"Science & Technology","author":"ITSub잇섭"}
+
+print(type(text))
--- utube_crawling/utube.py
+++ utube_crawling/utube.py
@@ -13,9 +13,9 @@ warnings.filterwarnings('ignore')
 
 
 chrome_options = webdriver.ChromeOptions()
-# chrome_options.add_argument('--headless')
-# chrome_options.add_argument('--no-sandbox')
-# chrome_options.add_argument('--disable-dev-shm-usage')
+chrome_options.add_argument('--headless')
+chrome_options.add_argument('--no-sandbox')
+chrome_options.add_argument('--disable-dev-shm-usage')
 driver = webdriver.Chrome('chromedriver.exe', chrome_options=chrome_options)
 
 #target of crawling
@@ -26,33 +26,42 @@ time.sleep(4)
 # 스크롤 내리기
 def scroll_down(driver):
     last_page_height = driver.execute_script("return document.documentElement.scrollHeight")
-    while True:
-        driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
-        time.sleep(3)
-        new_page_height = driver.execute_script("return document.documentElement.scrollHeight")
-        if new_page_height == last_page_height:
-            break
+    # while True:
+    driver.execute_script("window.scrollTo(0, document.documentElement.scrollHeight);")
+    time.sleep(3)
+    new_page_height = driver.execute_script("return document.documentElement.scrollHeight")
+        # if new_page_height == last_page_height:
+        #     break
         
 # 동영상제목
 html = driver.page_source
 soup = BeautifulSoup(html,'html.parser')
-title = soup.find("script",class_='style-scope ytd-player-microformat-renderer').get_text()
-title = eval(title)
-print(type(title))
-print(title['name'])
+title = soup.find("script",class_='style-scope ytd-player-microformat-renderer').get_text() #뽑을수 있는 데이터는 다뽑자
+# title = eval(title)
+# print(type(title))
+# print(title['name'])
 
-
-
-
-
-
-
-
-
-
+# 구독자 수
+counts = soup.find('yt-formatted-string',id='owner-sub-count').get_text()
+print(counts)
 # 채널명
+name = soup.find('div',class_='style-scope ytd-channel-name').find('a',class_='yt-simple-endpoint style-scope yt-formatted-string').get_text()
+print(name)
+
 # 조회수
+view_counts = soup.find('span',class_='view-count style-scope ytd-video-view-count-renderer').get_text().split(' ')[1]
+print(view_counts)
+
 # 영상 좋아요 수
+good_counts = soup.find('yt-formatted-string',class_='style-scope ytd-toggle-button-renderer style-text').get_text()
+print(good_counts)
+
+#스크롤 다운
+scroll_down(driver)
+
 # 댓글 크롤링
-# 작성자 크롤링
-# 공감 비공감 크롤링
+comments = soup.findall('ytd-comment-thread-renderer')
+for comment in comments:
+    
+## id
+## 공감
