3876cf4dfebd0139191ad2899e60e1cbe22237e2
hong <dlfemdrmq329@naver.com>
2022-09-24 13:01:19 +0900

    [Update] 동영상 카테고리 함수 & url 함수

--- utube_crawling/total_crawling.py
+++ utube_crawling/total_crawling.py
@@ -20,20 +20,105 @@ chrome_options = webdriver.ChromeOptions()
 chrome_options.add_argument('--no-sandbox')
 chrome_options.add_experimental_option('excludeSwitches', ['enable-logging'])
 chrome_options.add_argument('--disable-gpu')  
-# chrome_options.add_argument('--disable-dev-shm-usage')
-# chrome_options.add_argument('--disable-software-rasterizer')
 driver = webdriver.Chrome('chromedriver.exe', chrome_options=chrome_options)
 
 # url 
-"""
-1. https://www.youtube.com/c/DickHunter/featured
--> https://www.youtube.com/c/DickHunter/videos 변경
+# def url_transform():
+# 동영상 코너로 변환
+user_url = "https://www.youtube.com/c/DickHunter/featured"
+url_splited = user_url.split('/')
+if url_splited[-1] =='featured':
+    user_url = user_url.replace('featured', 'videos') 
+    print('user_url:',user_url)
+    
+driver.get(user_url) 
+time.sleep(5)
+# 영상 선택 기준
+# step 1) 업로드한 영상 + 정렬기준
 
-2. 정렬기준 클릭 후 3 Types 선택
+def uploaded_videos(sorting_value='인기 동영상'):
+    
+    #정렬기준 선택
+    driver.find_element_by_xpath('//*[@id="sort-menu"]/yt-sort-filter-sub-menu-renderer/yt-dropdown-menu').click()
+    time.sleep(1)
 
-3. URL 수집 
+    # 추가된 날짜(오래된순)
+    if sorting_value == '추가된 날짜(오래된순)':
+        driver.find_element_by_xpath('//*[@id="menu"]/a[2]').click()
+        time.sleep(1)
 
-4. df = one_crawling() 실행
+    # 추가된 날짜(최신순)
+    elif sorting_value == '추가된 날짜(최신순)':
+        driver.find_element_by_xpath('//*[@id="menu"]/a[3]').click()
+        time.sleep(1)
 
-5. df concat 실행
-"""
+    else:
+        pass #인기동영상(defalt value)
+
+def videos_select_options(selected_options='업로드한 동영상'):
+    
+    # 업로드된 동영상 칸 클릭
+    driver.find_element_by_xpath('//*[@id="label"]/paper-ripple').click()
+    
+    
+    # 전체동영상인 경우
+    if selected_options == '전체 동영상':       
+        #전체동영상클릭
+        driver.find_element_by_xpath('//*[@id="menu"]/a[1]/tp-yt-paper-item').click()
+        time.sleep(1)
+        
+    elif selected_options == '이전 실시간 스트림':
+        #이전 실시간 스트림 클릭
+        driver.find_element_by_xpath('//*[@id="menu"]/a[3]/tp-yt-paper-item').click()
+        time.sleep(1)
+    
+    else:
+        uploaded_videos()
+        time.sleep(1)
+
+# 스크롤 다운
+
+def scroll_down():
+    while True:
+        # 끝까지 스크롤 다운
+        driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
+
+        time.sleep(3)
+
+        # 스크롤 다운 후 스크롤 높이 다시 가져옴
+        new_height = driver.execute_script("return document.body.scrollHeight")
+        if new_height == last_height:
+            break
+        last_height = new_heigh
+
+#3. URL 수집 
+# def urls_crawling(counts=np.inf):
+urls = []
+soup = soup_find(driver)
+information_all = soup.find('div',{'id':'contents','class':'style-scope ytd-item-section-renderer'}).find_all('ytd-grid-video-renderer',class_='style-scope ytd-grid-renderer')
+for idx,inform in enumerate(information_all):
+    idx = idx+1 
+    url = inform.find('a',class_='yt-simple-endpoint inline-block style-scope ytd-thumbnail')['href']
+    url = "https://www.youtube.com"+url
+    urls.append(url)
+    print(url)
+    if counts == idx: 
+        break
+driver.close()
+    
+    
+    
+    
+    
+    
+# def main():
+# # df = one_crawling() 실행
+# total = pd.DataFrame()
+# for url in urls[::]:
+#     # 정보 긁어오기
+#     df = one_crawling(url)
+    
+#     # concat 실행
+#     total_df = pd.concat([total,df])
+    
+
